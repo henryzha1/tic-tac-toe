1daa1d9c90fa7530906378633c028f7d3f0b85c2
Henry Zhang <henz51111@gmail.com>
2024-01-24 22:19:32 -0800

    Finish step 1 of business logic. Game preferences UI is set as well

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,73 @@
-function getUserPreferences(e) {
+function Game(gameType, user1, botDifficulty = 0) {
+    this.gameType = gameType; //User or BOT
+    this.user1 = user1; // X or O
+    this.botDifficulty = botDifficulty; //Easy or Hard (Please select difficulty if User game)
+}
+
+function handleUserGameType(e) {
     e.preventDefault();
+    document.getElementById("one").innerText = "User 1: ";
+    document.getElementById("two").innerText = "User 2: ";
+
+    const user1Option = document.querySelector("#user1>select").value;
+    if(user1Option === "X") {
+        document.getElementById("one").append("X");
+        document.getElementById("two").append("O");
+    } else {
+        document.getElementById("one").append("O");
+        document.getElementById("two").append("X");
+    }
+    document.getElementById("one").removeAttribute("class");
+    document.getElementById("two").removeAttribute("class");
+    document.getElementById("start").removeAttribute("class");
+}
+
+function handleBotGameType(e) {
+    e.preventDefault();
+
+    document.getElementById("user1").removeAttribute("class");
+    document.getElementById("user1").addEventListener("change", handleUserGameType);
+}
+
+function handleStartSubmission() {
+    document.getElementById("start").disabled = true;
+    const gameType = document.querySelector("#gameType>select").value;
+    const user1 = document.getElementById("user1Option").value;
+    const bot = document.getElementById("botOption").value;
+    
+    const game = new Game(gameType, user1, bot);
+
+}
+
+function handleGameTypeSubmssion(e) {
+    e.preventDefault();
+
     document.getElementById("user1").setAttribute("class", "hidden");
+    document.getElementById("user1").removeEventListener("change", handleUserGameType);
     document.getElementById("user1Option").selectedIndex = 0;
     document.getElementById("one").setAttribute("class", "hidden");
     document.getElementById("two").setAttribute("class", "hidden");
+    document.getElementById("bot").setAttribute("class", "hidden");
+    document.getElementById("bot").removeEventListener("change", handleBotGameType);
+    document.getElementById("botOption").selectedIndex = 0;
+    document.getElementById("start").setAttribute("class", "hidden");
+    document.getElementById("start").removeEventListener("click", handleStartSubmission);
 
 
-    const game = document.querySelector("#gameType>select").value;
-    if(game === "User") {
+    const gameType = document.querySelector("#gameType>select").value;
+    if(gameType === "User") {
         document.getElementById("user1").removeAttribute("class");
-        document.getElementById("user1").addEventListener("change", function(e) {
-            e.preventDefault();
-            document.getElementById("one").innerText = "User 1: ";
-            document.getElementById("two").innerText = "User 2: ";
-
-            document.getElementById("one").setAttribute("class", "hidden");
-            document.getElementById("two").setAttribute("class", "hidden");
-
-            const user1Option = document.querySelector("#user1>select").value;
-            if(user1 === "X") {
-                document.getElementById("one").append("X");
-                document.getElementById("two").append("O");
-            } else {
-                document.getElementById("one").append("O");
-                document.getElementById("two").append("X");
-            }
-            document.getElementById("one").removeAttribute("class");
-            document.getElementById("two").removeAttribute("class");
-        })
+        document.getElementById("user1").addEventListener("change", handleUserGameType);
     } else {
         document.getElementById("bot").removeAttribute("class");
-        const botOption = document.getElementById("botOption").value;
-        document.getElementById("bot").addEventListener("change", function(e) {
-            document.getElementById("user1").removeAttribute("class");
-            const user1Option = document.getElementById("user1").value;
-        });
+        document.getElementById("bot").addEventListener("change", handleBotGameType)
     }
+
+    document.getElementById("start").addEventListener("click", handleStartSubmission);
 
 }
 
 
 window.addEventListener("load", function() {
-    document.getElementById("gameType").addEventListener("change", function(e) {
-        getUserPreferences(e);
-        
-
-    })
+    document.getElementById("gameType").addEventListener("change", handleGameTypeSubmssion);
 });
